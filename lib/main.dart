1bec817319d001044b91a7d707c1dab2157ebb17
Eddie Li <lh.eddieli@gmail.com>
2019-10-09 07:33:12 -0400

    news api is added

--- lib/main.dart
+++ lib/main.dart
@@ -1,100 +1,211 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import './ui/create_profile.dart';
+import 'dart:async';
+import 'dart:convert';
+import "package:http/http.dart" as http;
+import 'package:intl/intl.dart';
 
-void main() => runApp(new MaterialApp(
-  home: new HomeScreen(),
-  debugShowCheckedModeBanner: false,
-));
+var now = new DateTime.now();
+var formatter = new DateFormat('yyyy-mm-dd');
+String currentDate = formatter.format(now);
+
+// All news bout bitcoin
+final String _apiUrl1 = "https://newsapi.org/v2/everything?q=bitcoin&from="
+  + currentDate +
+"&sortBy=popularity&apiKey=feb98c88495b4f2781002bc3f403ae4e";
+
+// All new about business
+final String _apiUrl2 = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=feb98c88495b4f2781002bc3f403ae4e";
 
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key key}) : super(key: key);
+final String _apiUrl3 = "https://newsapi.org/v2/everything?q=apple&from="
++ currentDate +
+"&to="
++ currentDate +
+"&sortBy=popularity&apiKey=feb98c88495b4f2781002bc3f403ae4e";
 
+
+// Variables to store the data
+List _provider1;
+List _provider2;
+List _provider3;
+
+List _provider;
+String _headlines = "BitCoin News";
+
+
+void main() async{
+  try {
+    _provider1 = await fetchData(_apiUrl1);
+    _provider2 = await fetchData(_apiUrl2);
+    _provider3 = await fetchData(_apiUrl3);
+  } catch (e) {
+    print(e);
+  }
+
+  _provider = _provider1;
+  
+  runApp (new MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    return Container(
+       child: Stack(
+         fit: StackFit.expand,
+         children: <Widget> [
+           Image.asset("images/background.jpg", fit: BoxFit.cover,),
 
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
-      statusBarIconBrightness: Brightness.light
-    ));
+           Scaffold(
+             drawer: Drawer(
+               child: ListView(
+                 children: <Widget> [
+                   DrawerHeader(
+                     child: Text("CHOOSE A NEWS PROVIDER",
+                      style: TextStyle(color: Colors.white, fontFamily: 'Times New Roman',
+                      fontSize: 18.0),
+                     ),
+                     decoration: BoxDecoration(color: Colors.blue)
+                   ),
 
-    return Scaffold(
-      body: Stack(
-        children: <Widget> [
-          new Image.asset(
-            'images/background.jpg',
-            fit: BoxFit.cover,
-            height: double.infinity,
-            width: double.infinity,
-            alignment: Alignment.center
-            ),
+                   ListTile(
+                     title: Text("Bitcoin News", style: TextStyle(
+                       fontFamily: 'Times New Roman', fontSize: 18.0, fontStyle: FontStyle.italic
+                     )),
+                     subtitle: Text("News Articles Mentioning BitCoin", style: TextStyle(
+                       fontFamily: 'Times New Roman', fontSize: 16.0, fontStyle: FontStyle.italic
+                     )),
 
-            Container(
-              alignment: Alignment.topCenter,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(padding: EdgeInsets.all(15.0),),
+                     onTap: () {
+                       setState(() {
+                         _provider = _provider1;
+                         _headlines = "BitCoin News";
+                       });
+                     }
+                   ),
 
-                  Text(
-                    "Teacher's \n Corner",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 36.0, fontWeight: FontWeight.w500,
-                      fontFamily: 'Chalkduster',
-                      color: Colors.green,
-                    )
-                  ),
+                   ListTile(
+                     title: Text("Top Business News", style: TextStyle(
+                       fontFamily: 'Times New Roman', fontSize: 18.0, fontStyle: FontStyle.italic
+                     )),
+                     subtitle: Text("News Articles Mentioning Top Business", style: TextStyle(
+                       fontFamily: 'Times New Roman', fontSize: 16.0, fontStyle: FontStyle.italic
+                     )),
+                     
+                     onTap: () {
+                       setState(() {
+                         _provider = _provider2;
+                         _headlines = "Top Business News";
+                       });
+                     }
+                   ),
 
-                  Padding(padding: EdgeInsets.all(15.0),),
+                    ListTile(
+                     title: Text("Apple News", style: TextStyle(
+                       fontFamily: 'Times New Roman', fontSize: 18.0, fontStyle: FontStyle.italic
+                     )),
+                     subtitle: Text("News Articles Mentioning Apple", style: TextStyle(
+                       fontFamily: 'Times New Roman', fontSize: 16.0, fontStyle: FontStyle.italic
+                     )),
+                     
+                     onTap: () {
+                       setState(() {
+                         _provider = _provider3;
+                         _headlines = "Apple News";
+                       });
+                     }
+                   ),
 
-                  Text(
-                    "An app to meet other teachers and share resources",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 18.0,
-                      fontFamily: 'Roboto',
-                      color: Colors.white,
-                    )
-                  ),
 
-                  Padding(padding: EdgeInsets.all(60.0),),
+                 ],
+               )
+             ),
+             
+             appBar: AppBar(
+                centerTitle: true,
+                title: Text(_headlines.toUpperCase(),
+                  style: TextStyle(fontFamily: 'Times New Roman')),
+                elevation: 3.0,
+                backgroundColor: Colors.blue.withOpacity(0.5),
+             ),
+             
+             body: Center(
+               child: ListView.builder(
+                 padding: const EdgeInsets.all(12.0),
+                 itemCount: _provider.length,
+                 itemBuilder: (BuildContext context, int position) {
+                   return Card(
+                     child: Column(
+                       mainAxisSize: MainAxisSize.min,
+                       children: <Widget> [
+                         ListTile(
+                           title: CachedNetworkImage(
+                             imageUrl: "${_provider[position]['urlToImage']}",
+                            //  placeholder: CircularProgressIndicator(),
+                            //  errorWidget: Icon(Icons.error),
+                           ),
 
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget> [
-                      OutlineButton(
-                        child: Text("Sign up", style: TextStyle(color: Colors.white, fontSize: 20.0)),
-                        padding: const EdgeInsets.all(5.0),
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-                        onPressed: () => 
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => CreateProfile())
-                          )),
+                           subtitle: Text("${_provider[position]['title']}",
+                            style: TextStyle(fontSize: 16.0,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: "Times New Roman"),
+                           ),
+                           onTap: () => _showNewsSnippet(context, _provider[position]['description']),
+                           
+                         )
+                       ]
+                     )
+                   );
+                 },
+               ),
+             ),
 
-                      OutlineButton(
-                        child: Text("Sign in", style: TextStyle(color: Colors.white, fontSize: 20.0)),
-                        padding: const EdgeInsets.all(5.0),
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-                        onPressed: () => debugPrint("Sign in pressed")
-                      ),
-                    ],
-                  ),
+             backgroundColor: Colors.transparent
+           )
 
-                  Padding(padding: EdgeInsets.all(60.0),),
 
-                  InkWell(
-                    onTap: () => debugPrint("Skip Button Pressed"),
-                    child: Text("Skip this for now!>>>",
-                    style: TextStyle(
-                      fontSize: 18.0, fontFamily: 'Roboto', color: Colors.white),
-                    ))
-                ]
-              )
-            )
-        ]
-      ),
+         ]
+       ),
     );
   }
+}
+
+// Functions
+Future<List>fetchData(String apiUrl) async {
+  http.Response response = await http.get(apiUrl);
+  return json.decode(response.body)['articles']; 
+}
+
+void _showNewsSnippet(BuildContext context, String snippet) {
+  var alert = AlertDialog(
+    title: Text("Headlines", style: TextStyle(
+      fontSize: 16.0, fontFamily: 'Times New Roman', fontWeight: FontWeight.w500
+    ),),
+
+    content: Text(snippet),
+
+    actions: <Widget>[
+      FlatButton(
+        child: Text("Dismiss", style: TextStyle(
+          fontSize: 16.0, fontFamily: 'Times New Roman', fontStyle: FontStyle.italic
+        )),
+
+        onPressed: () => Navigator.pop(context),
+      )
+    ],
+    
+  );
+
+
+  showDialog(context: context, builder: (context) => alert);
 }
